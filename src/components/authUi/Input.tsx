f3493b4f8c02101c7920dc9cea77ed69b6ebd00a
Bouih32 <enamto.bouih@gmail.com>
2025-01-21 15:58:16 +0000

    feat: add login form validation

--- src/components/authUi/Input.tsx
+++ src/components/authUi/Input.tsx
@@ -1,22 +1,43 @@
+import { cn } from "@/libs/utils";
 import { ReactElement } from "react";
+import { FieldValues, Path, UseFormRegister } from "react-hook-form";
 
-type InputProps = {
-  icon: ReactElement;
+type InputProps<T extends FieldValues> = {
+  register: UseFormRegister<T>;
+  error: string | undefined;
+  name: Path<T>;
   placeholder: string;
   type: string;
+  icon: ReactElement;
 };
 
-export default function Input({ icon, placeholder, type }: InputProps) {
+export default function Input<T extends FieldValues>({
+  icon,
+  placeholder,
+  name,
+  type,
+  register,
+  error,
+}: InputProps<T>) {
   return (
-    <div className="flex items-center justify-between border-b pb-[9px] pt-[15px] placeholder:text-black60 md:border-none md:border-black30 md:bg-white90 md:px-3 md:py-2.5">
-      <input
-        type={type}
-        name="email"
-        id="email"
-        className="w-full rounded-sm bg-transparent caret-main outline-none placeholder:text-black30"
-        placeholder={placeholder}
-      />
-      <div className="text-[21px] text-black60"> {icon}</div>
-    </div>
+    <section className="">
+      <div
+        className={cn(
+          "flex items-center justify-between border-b border-black30 pb-[9px] pt-[15px] placeholder:text-black60 md:bg-white90 md:px-3 md:py-2.5",
+          { "border-error md:border-b": error },
+          { "md:border-none": !error },
+        )}
+      >
+        <input
+          type={type}
+          {...register(name)}
+          id={name}
+          className="w-full rounded-sm bg-transparent caret-main outline-none placeholder:text-black30"
+          placeholder={placeholder}
+        />
+        <div className="text-[21px] text-black60"> {icon}</div>
+      </div>
+      {error && <p className="text-error">{error}</p>}
+    </section>
   );
 }
