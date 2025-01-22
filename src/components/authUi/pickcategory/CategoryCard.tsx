2fa33cdcad730ce60feaa9c7da7af44d2dcb1381
Bouih32 <enamto.bouih@gmail.com>
2025-01-22 21:55:07 +0000

    feat: implement category selection logic with state management in PickCategory and CategoryCard components

--- src/components/authUi/pickcategory/CategoryCard.tsx
+++ src/components/authUi/pickcategory/CategoryCard.tsx
@@ -3,21 +3,27 @@
 import Image from "next/image";
 import placeholder from "@/images/categoryImage.png";
 import { FaCheckCircle } from "react-icons/fa";
-import { useState } from "react";
 import { cn } from "@/libs/utils";
 
-export default function CategoryCard() {
-  const [clicked, setClicked] = useState(false);
+type CategoryCardType = {
+  handleSelect: (ele: number) => void;
+  index: number;
+  selcted: number[];
+};
 
-  const handleClick = () => {
-    setClicked((prev) => !prev);
-  };
+export default function CategoryCard({
+  handleSelect,
+  index,
+  selcted,
+}: CategoryCardType) {
+  const isSelected = selcted.includes(index);
+
   return (
     <div
-      onClick={handleClick}
+      onClick={() => handleSelect(index)}
       className={cn(
         "before:bg-categoryGrad relative h-[122px] w-[160px] cursor-pointer overflow-hidden rounded-[3px] border-main transition-all duration-75 ease-in before:absolute before:bottom-0 before:left-0 before:h-full before:w-full hover:border-[2px] md:h-[140px] md:w-[211px] xl:w-[184px]",
-        clicked && "border-[2px]",
+        isSelected && "border-[2px]",
       )}
     >
       <Image
@@ -28,7 +34,7 @@ export default function CategoryCard() {
       <div className="absolute bottom-0 left-0 w-full pb-2 text-center">
         <h2 className="z-50 text-12lg text-white90 md:text-16lg">Design</h2>
       </div>
-      {clicked && (
+      {isSelected && (
         <FaCheckCircle className="absolute right-2 top-2 text-main" />
       )}
     </div>
--- src/components/authUi/pickcategory/PickCategory.tsx
+++ src/components/authUi/pickcategory/PickCategory.tsx
@@ -1,8 +1,28 @@
+"use client";
+
 import Button from "@/components/ui/Button";
 import CategoryCard from "./CategoryCard";
 import { nanoid } from "nanoid";
+import { useState } from "react";
 
 export default function PickCategory() {
+  const [selcted, setSelected] = useState<number[]>([]);
+  const handleSelect = (ele: number) => {
+    setSelected((prev) => {
+      if (prev.includes(ele)) {
+        // If the element is already selected, remove it
+        return prev.filter((cat) => cat !== ele);
+      } else if (prev.length < 3) {
+        // If the length is less than 3, add the element
+        return [...prev, ele];
+      }
+      // Otherwise, return the previous state (no changes)
+      return prev;
+    });
+
+    console.log(selcted);
+  };
+
   return (
     <section className="space-y-6 px-5 py-6 md:w-[944px] xl:w-[834px] xl:space-y-[32px] xl:py-10">
       <p className="text-center text-16lg md:text-20lg">
@@ -10,7 +30,12 @@ export default function PickCategory() {
       </p>
       <section className="grid grid-cols-2 gap-[15px] md:grid-cols-4 md:gap-5">
         {Array.from({ length: 8 }).map((_, index) => (
-          <CategoryCard key={nanoid()} />
+          <CategoryCard
+            key={nanoid()}
+            handleSelect={handleSelect}
+            index={index}
+            selcted={selcted}
+          />
         ))}
       </section>
       <Button classname="xl:w-[386px] md:w-[288px] w-full mx-auto">Done</Button>
